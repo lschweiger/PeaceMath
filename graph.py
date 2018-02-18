1bc216f23b94e766dc1d47ce38411f3a791047ef
Launy <launy.schweiger52@qmail.cuny.edu>
2018-02-17 19:58:59 -0500

    added comments to file

--- graph.py
+++ graph.py
@@ -8,7 +8,7 @@ Created on Sat Feb 17 12:04:06 2018
 import data
 from indigoclass_V_3efix import *
 from indigo_V_3efix import *
-rang=5
+rang=100
 storev=[0] * rang
 storec=[0] * rang
 
@@ -19,12 +19,17 @@ class CG:
         pass_data.jvalue = 0.0
         pass_data.jfix=3
         for i in range(rang):
+            #zzz.recalculate(pass_data) make the gui recalculate 
             zzz.recalculate(pass_data)
+            #App.sumpn(pass_data) runs the sumpn from indigoclass
             App.sumpn(pass_data)
+            #stores values of every run, so it can be used in graphing
             storec[i]=pass_data.jvalue
             storev[i]=pass_data.avg
+            #plt.close() will close all opened figures, so as not to cloter the screen
             plt.close()
             if(i!=rang-1):
+                #resets z[-1] to ic for next recalcution and increments constants held 
                 zzz.resetIC()
                 pass_data.jvalue += 0.01
            
