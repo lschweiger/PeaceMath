1e46e8780f6181f43cf7d96f0a0c80dff67dd3bd
Launy <lschweiger939@gmail.com>
2018-05-28 00:46:12 -0400

    further work on importing from json file

--- json/json.py
+++ json/json.py
@@ -27,3 +27,24 @@ class Json:
             for i in out['elements']:
                 Json.factors[i['attributes']['label']]=i['_id']
     
+            for j in range(0,From):
+                for k in range(0,From):
+                    if((out['connections'][j]['from'])==out['connections'][k]['from']):
+                        Json.tolist.append(out['connections'][k]['to'])
+                        Json.connections[out['connections'][j]['from']]=Json.tolist
+                Json.tolist=[]
+            for l in range(0,(len(out['maps'][0]['elements']))):
+                Json.locations.append(out['maps'][0]['elements'][l]['position'])
+                Json.position[out['maps'][0]['elements'][l]['element']]=Json.locations[l]
+            Json.locations=[]
+    def Sort():
+        Json.sortposition=sorted(Json.position.items(),key=operator.itemgetter(0))
+
+def main():
+    Json.Import()
+    Json.Sort()
+if __name__=='__main__':
+    main()
+    print(Json.sortposition)
+    print(Json.factors)
+    print(Json.connections)
